df3ecba432cf0bd9ecf12837d3df4e42e0efa3a7
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2023-04-27 21:23:59 +0000

    added values section

--- jobhunt/src/components/JobsSec/Jobs.jsx
+++ jobhunt/src/components/JobsSec/Jobs.jsx
@@ -2,46 +2,27 @@ import { BsClock } from "react-icons/bs";
 
 import logo from "../../assets/keras-logo.png";
 
+const jobsData = [
+  {
+    id: 2344,
+    title: "Django Backend Developer",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+];
+
 const Jobs = () => {
   return (
     <section className=" jobs__sec">
-      <div className="jobs__container grid grid-cols-1 mt-10 gap-5 md:grid-cols-4">
-        <article className=" p-2 bg-white rounded-md shadow-md cursor-pointer">
-          <div className="top flex  justify-between p-2 text-l  items-center ">
-            <h2 className=" flex flex-col font-semibold ">
-              Frontend Developer
-              <span className="job__location text-gray-400"> Accra</span>
-            </h2>
-            <p className=" flex items-center gap-2 time__posted text-gray-400">
-              <span>
-                <BsClock />
-              </span>
-              Now
-            </p>
-          </div>
+      <div className="sec__heading flex flex-col text-center mt-14 uppercase font-semibold ">
+        <span className=" text-xs text-blueColor">
+          Find a suitable Job here
+        </span>
+        <h1 className=" text-3xl text-gray-400">jobs Available </h1>
+      </div>
 
-          <div className="content border-t-slate-200 border-t-2 p-2">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptatem molestias repellat error assumenda non beatae modi
-              dolorem iure sequi quibusdam?
-            </p>
-            <div className="company flex items-center gap-2 mt-3">
-              <img
-                src={logo}
-                alt=""
-                className=" company__logo w-8 h-8 rounded-full"
-              />
-              <span className=" company__name"> WewoliTech </span>
-            </div>
-          </div>
-
-          <a href="#" className=" btn inline-block mt-3 w-[100%] text-center">
-            Apply Now
-          </a>
-        </article>
-        <article className=" p-2 bg-white rounded-md shadow-md">
-          <div className="top flex  justify-between p-2 text-l  items-center">
+      <div className="jobs__container grid grid-cols-1 mt-5 gap-5 md:grid-cols-2 lg:grid-cols-4">
+        <article className=" p-2 bg-white rounded-md shadow-md cursor-pointer text-sm  transition-all delay-300  hover:bg-slate-200  hover:scale-105  ">
+          <div className="top flex  justify-between p-2   items-center ">
             <h2 className=" flex flex-col font-semibold ">
               Frontend Developer
               <span className="job__location text-gray-400"> Accra</span>
--- jobhunt/src/components/Value/Value.jsx
+++ jobhunt/src/components/Value/Value.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import { SiSimplenote } from "react-icons/si";
+import { FaParachuteBox } from "react-icons/fa";
+import { BsShieldFillCheck } from "react-icons/bs";
 
 const Value = () => {
-  return <div>Value</div>;
+  return (
+    <section className="values__sec mt-[10rem]">
+      <div className="sec__heading flex flex-col text-center mt-14 uppercase font-semibold ">
+        <span className=" text-xs text-blueColor">what we stand for</span>
+        <h1 className=" text-3xl text-gray-400">core values </h1>
+      </div>
+      <div className="values__container grid md:grid-cols-2 lg:grid-cols-3 gap-[5rem] items-center mt-3">
+        <div className="value flex  flex-col  justify-between  hover:bg-slate-100 hover:cursor-pointer rounded-md p-3  transition-all delay-150 ">
+          <div className=" flex  gap-[1.5rem] items-center">
+            <div className="value__img">
+              <SiSimplenote className=" text-blue-500 text-2xl w-[2.5rem] h-[2.5rem] rounded-full bg-blue-200 p-2" />
+            </div>
+            <span className=" text-2xl font-semibold text-textColor">
+              Simplicity
+            </span>
+          </div>
+          <p className="text-sm pt-2">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet
+            obcaecati a error, praesentium iure odit fugiat nemo corporis sunt
+            facere?
+          </p>
+        </div>
+        <div className="value flex  flex-col  justify-between  hover:bg-slate-100 hover:cursor-pointer rounded-md p-3  transition-all delay-150 ">
+          <div className=" flex  gap-[1.5rem] items-center">
+            <div className="value__img">
+              <FaParachuteBox className=" text-pink-600 text-2xl w-[2.5rem] h-[2.5rem] rounded-full bg-pink-200 p-2" />
+            </div>
+            <span className=" text-2xl font-semibold text-textColor">
+              Simplicity
+            </span>
+          </div>
+          <p className="text-sm pt-2">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet
+            obcaecati a error, praesentium iure odit fugiat nemo corporis sunt
+            facere?
+          </p>
+        </div>
+        <div className="value flex  flex-col  justify-between  hover:bg-slate-100 hover:cursor-pointer rounded-md p-3  transition-all delay-150 ">
+          <div className=" flex  gap-[1.5rem] items-center">
+            <div className="value__img">
+              <BsShieldFillCheck className=" text-amber-500 text-2xl w-[2.5rem] h-[2.5rem] rounded-full bg-amber-200 p-2" />
+            </div>
+            <span className=" text-2xl font-semibold text-textColor">
+              Simplicity
+            </span>
+          </div>
+          <p className="text-sm pt-2">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet
+            obcaecati a error, praesentium iure odit fugiat nemo corporis sunt
+            facere?
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Value;
